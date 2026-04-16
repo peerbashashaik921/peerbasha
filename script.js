a62f89b717f6a550d1294350f236fe8fc2a9f6f2
Peerbasha <peerbashashaik921@gmail.com>
2026-04-16 19:26:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,11 @@
-let display = document.getElementById('display');
-
-function appendToDisplay(input) {
-    if (display.innerText === '0') {
-        display.innerText = input;
-    } else {
-        display.innerText += input;
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
     }
-}
+  });
+});
 
-function clearDisplay() {
-    display.innerText = '0';
-}
-
-function deleteLast() {
-    if (display.innerText.length > 1) {
-        display.innerText = display.innerText.slice(0, -1);
-    } else {
-        display.innerText = '0';
-    }
-}
-
-function calculate() {
-    try {
-        display.innerText = eval(display.innerText);
-    } catch (error) {
-        display.innerText = "Error";
-    }
-}
+document.querySelectorAll('.fade-in').forEach(el => {
+  observer.observe(el);
+});
